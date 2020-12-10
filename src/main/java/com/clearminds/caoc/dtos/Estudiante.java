b1d260ac2f08c3c1322b8ffbcbe4795901cc1920
caobandoc <obandocarlosandres@gmail.com>
2020-12-10 09:39:58 -0500

    Agregando edad a estudiantes

--- src/main/java/com/clearminds/caoc/dtos/Estudiante.java
+++ src/main/java/com/clearminds/caoc/dtos/Estudiante.java
@@ -4,20 +4,30 @@ public class Estudiante {
 	private String nombre;
 	private String apellido;
 	private int id;
+	private int edad;
 	
 	
 	public Estudiante() {
 	}
-	public Estudiante(String nombre, String apellido, int id) {
+	
+	public Estudiante(String nombre, String apellido, int id, int edad) {
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.id = id;
+		this.edad = edad;
 	}
-	public Estudiante(String nombre, String apellido) {
+	public Estudiante(String nombre, String apellido, int edad) {
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.edad = edad;
 	}
 	
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 	public String getNombre() {
 		return nombre;
 	}
@@ -38,6 +48,7 @@ public class Estudiante {
 	}
 	@Override
 	public String toString() {
-		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + "]";
+		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + ", edad=" + edad + "]";
 	}
+
 }
